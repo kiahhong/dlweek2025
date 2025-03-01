8024c67d88f20363bd65bc698eeae8e1efe95c80
hfph <chiawl012@gmail.com>
2025-03-01 16:46:08 +0800

    remove old code

--- backend/app/artifacts/GenImgClassifier.py
+++ backend/app/artifacts/GenImgClassifier.py
@@ -25,21 +25,3 @@ class GenImgClassifier:
         class_names = ['real', 'fake']
         label = class_names[pred]
         return label
-
-
-
-
-def predict_image(image_path):
-    """Predicts whether an image is real or AI-generated."""
-    img = Image.open(image_path).convert("RGB")
-
-    # Apply the same transforms as validation (resize, tensor, normalize)
-    img_tensor = val_transforms(img).unsqueeze(0).to(device)
-    with torch.no_grad():
-        outputs = model(img_tensor)
-        logits = outputs.logits
-        pred = torch.argmax(logits, dim=1).item()
-    label = class_names[pred]
-    return label
-
-print(predict_image("/content/drive/MyDrive/testing - Copy/test/REAL/0003 (5).jpg"))  # prints "real" or "fake"
